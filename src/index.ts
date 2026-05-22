cc624ec74416f15a203e4f3bfbac054c099a2af0
raka1 <p.raka54@ymail.com>
2026-05-22 15:55:29 +0700

    Redis DB

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
   password: process.env.REDIS_PASSWORD || undefined,
+  db: Number(process.env.REDIS_DB) || 0,
 })
 
 // Middleware
